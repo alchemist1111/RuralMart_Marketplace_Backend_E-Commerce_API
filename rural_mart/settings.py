340a4423aa590933db5205287ad5a9976a5d43b4
alchemist1111 <emoyoeugine@gmail.com>
2025-11-14 12:42:14 +0300

    Create a new accounts app to handle user authentication and authorization

--- rural_mart/settings.py
+++ rural_mart/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local Apps
+    'accounts',
+    
+    # Third Party Apps
 ]
 
 MIDDLEWARE = [
